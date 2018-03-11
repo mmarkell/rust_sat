55a7361a70ddf3d27401c4c3e715ef9ca0376a56
mm116 <michael_markell@brown.edu>
2018-03-11 16:43:05 -0400

    cleaning up main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,16 @@ fn main() {
         let mut line = String::new();
         match stdin().read_line(&mut line) {
             Ok(0) => break,
-            Ok(1) => test += &(line.clone()),
-            Ok(length) => {
-                if line == "\n" {
-                    break;
-                } else {
-                    test += &(line.clone());
-                }
-            },
+            Ok(_) => test += &(line.clone()),
             _ => break,
         }
     }
+
+    /*
+        I use a stack of tests so that I could test multiple tests rapidly
+        by adding them to a stack and running them one after the other.
+    */
+
     tests_to_run.push(test[..test.len() -1].to_string());
     let tests: Vec<(input::FORMULA)> = input::get_tests(tests_to_run);
     solver::solve_formulas(tests);
